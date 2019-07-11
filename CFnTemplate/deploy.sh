199018ac9376b2814b5137bf3e866b372b922347
moqrin3 <moqrin3@gmail.com>
2019-07-11 17:50:16 +0900

    test

---
+++ CFnTemplate/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source envfile
+
+TEMPLATE=demo.template
+
+TEMPLATE_NAME=${TEMPLATE}.yml
+STACK_NAME=${NameTagPrefix}-codedeploy-stack
+
+# テンプレートの実行
+aws cloudformation deploy --stack-name ${STACK_NAME} --template-file ${TEMPLATE_NAME} \
+  --parameter-overrides \
+  NameTagPrefix=${NameTagPrefix} \
+  GitHubRepositoryName=${GitHubRepositoryName} \
+  GitHubAccountName=${GitHubAccountName} \
+  GitHubSecret=${GitHubSecret} \
+  Branch=${Branch} \
+  ApplicationName=${ApplicationName} \
+  DeploymentGroupName=${DeploymentGroupName} \
+  PipelineName=${PipelineName} \
+  --capabilities CAPABILITY_NAMED_IAM
+
+exit 0
